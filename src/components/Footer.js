5c9d807d77402c59d32a02a4d792050ae73f2565
VikrantAhiwale <vikrantahiwale@gmail.com>
2024-10-26 22:12:36 +0530

    feat:added all the page with backgroud image

---
+++ src/components/Footer.js
@@ -0,0 +1,70 @@
+import "./FooterStyles.css";
+import React from "react";
+import {
+  FaHome,
+  FaPhone,
+  FaMailBulk,
+  FaFacebook,
+  FaTwitter,
+  FaLinkedin,
+} from "react-icons/fa";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="footer-container">
+        <div className="left">
+          <div className="location">
+            <FaHome size={20} style={{ color: "#fff", marginRight: "2rem" }} />
+            <div>
+              <p>90,Bethel House Chelekere, Banglore</p>
+              <p>KA,India</p>
+            </div>
+          </div>
+          <div className="phone">
+            <h4>
+              <FaPhone
+                size={20}
+                style={{ color: "#fff", marginRight: "2rem" }}
+              />
+              +91 9325307637
+            </h4>
+          </div>
+          <div className="email">
+            <h4>
+              <FaMailBulk
+                size={20}
+                style={{ color: "#fff", marginRight: "2rem" }}
+              />
+              vikrantahiwale71@gmail.com
+            </h4>
+          </div>
+        </div>
+        <div className="right">
+          <h4>About</h4>
+          <p>
+            A Software Engineer with 2+ years Skilled in Java, SpringBoot,
+            Database, Angular and React building robust web applications and
+            optimizing backend performance
+          </p>
+          <div className="social">
+            <FaFacebook
+              size={30}
+              style={{ color: "#fff", marginRight: "1rem" }}
+            />
+            <FaTwitter
+              size={30}
+              style={{ color: "#fff", marginRight: "1rem" }}
+            />
+            <FaLinkedin
+              size={30}
+              style={{ color: "#fff", marginRight: "1rem" }}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
--- src/routes/About.js
+++ src/routes/About.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import Heroimage2 from "../components/Heroimage2";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div>
+      <Navbar />
+      <Heroimage2 heading="ABOUT" text="Software Engineer at Speridian Technologies"/>
+      <Footer />
+    </div>
+  );
+};
 
-export default About
+export default About;
