23889dc6ab0c325e55c69aef248d7230157bb90c
VikrantAhiwale <vikrantahiwale@gmail.com>
2024-10-27 15:47:36 +0530

    added resume

--- src/components/AboutContent.js
+++ src/components/AboutContent.js
@@ -1,18 +1,27 @@
 import "./AboutContentStyles.css";
 import React from "react";
-import { Link } from "react-router-dom";
 import springboot from "../assets/javaspringboot.png";
-import react from "../assets/react.png";
 
 const AboutContent = () => {
   return (
     <div className="about">
       <div className="left">
         <h1>Who Am I...?</h1>
-        <p>Software Engineeer at Speridian Tech</p>
-        <Link to="/contact">
-          <div className="btn">Contact</div>
-        </Link>
+        <p>
+          Software Engineer with a Bachelor's from AISSMS College of
+          Engineering, specializing in Java backend development. Currently at
+          Speridian Technologies, I contribute to impactful healthcare and
+          financial projects using Java Spring Boot, Angular, and React. I’m
+          focused on advancing in cloud-native tech, microservices, and
+          AI-driven solutions
+        </p>
+        <a
+          href={`${process.env.PUBLIC_URL}/vikrant-ahiwale-sdev1.pdf`}
+          download
+          className="btn"
+        >
+          Download Resume
+        </a>
       </div>
 
       <div className="right">
@@ -20,9 +29,7 @@ const AboutContent = () => {
           <div className="img-stack top">
             <img src={springboot} className="img" alt="true"></img>
           </div>
-          <div className="img-stack bottom">
-            <img src={react} className="img" alt="true"></img>
-          </div>
+          <div className="img-stack bottom"></div>
         </div>
       </div>
     </div>
