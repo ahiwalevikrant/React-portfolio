231d4e2d5b64dbc3b03a87d2ed82efaa21c20540
VikrantAhiwale <vikrantahiwale@gmail.com>
2024-10-27 15:23:12 +0530

    Conact page added

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -2,12 +2,14 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Heroimage2 from "../components/Heroimage2";
+import  Form from "../components/Form";
 
 const Contact = () => {
   return (
     <div>
       <Navbar />
       <Heroimage2 heading="CONTACT" text="Let's Connect" />
+      <Form/>
       <Footer />
     </div>
   );
